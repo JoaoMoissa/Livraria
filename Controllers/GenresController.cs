965139a0eeef6f54e6ee681d0099fab1dda32a24
joao <joaomoissapersonal@gmail.com>
2024-11-12 16:52:25 -0300

    Tela de Erro

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -1,6 +1,9 @@
 ﻿using Bookstore.Data;
 using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Bookstore.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Bookstore.Controllers
 {
@@ -13,14 +16,53 @@ namespace Bookstore.Controllers
             _service = service;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(_service.FindAll());
+            List<Genre> genres = await _service.FindAllAsync();
+            return View(genres);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Genre genre)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _service.InsertAsync(genre);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não fornecido" });
+            } 
+            var obj = await _service.FindByIdAsync(id.Value);
+            if (obj is null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado" });
+            }
+            return View(obj);
+        }
+
+        public IActionResult Error(string? message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
--- Models/Genre.cs
+++ Models/Genre.cs
@@ -4,9 +4,10 @@ namespace Bookstore.Models
 {
     public class Genre
     {
-        public int Id{ get; set; } 
+        public int Id{ get; set; }
 
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get; set; }
 
         //public ICollection<Book> Books { get; set; } = new List<Book>();
